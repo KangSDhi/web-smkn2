2dc2913cca42328512369641b8bff66321b45755
KangSDhi <kangteknisi@gmail.com>
2025-08-06 13:53:09 +0700

    feat(dev): menambahkan fitur upload gambar artikel

--- resources/js/views/author/article/AddArticle.vue
+++ resources/js/views/author/article/AddArticle.vue
@@ -1,6 +1,6 @@
 <script>
 import BaseLayout from '../BaseLayout.vue';
-import { QuillEditor } from '@vueup/vue-quill';
+import {QuillEditor} from '@vueup/vue-quill';
 import '@vueup/vue-quill/dist/vue-quill.snow.css';
 import axios from "axios";
 
@@ -10,50 +10,92 @@ export default {
         BaseLayout,
         QuillEditor
     },
-    data(){
+    data() {
         return {
             token: localStorage.getItem("jwt"),
             title: null,
             body: null,
             user_id: localStorage.getItem("id_user"),
-            image: null,
-            imageUrl: null
+            imageCover: null,
+            imageCoverUrl: null,
+            imageArticle: [],
+            imageArticleUrl: []
         }
     },
     methods: {
-        onFileChange(event) {
+        onFileChangeCoverFile(event) {
             const file = event.target.files[0];
             if (file && file.type.startsWith('image/')) {
-                this.image = file;
+                this.imageCover = file;
 
                 const reader = new FileReader();
                 reader.onload = (e) => {
-                    this.imageUrl = e.target.result;
+                    this.imageCoverUrl = e.target.result;
                 };
 
                 reader.readAsDataURL(file);
             } else {
-                this.image = null;
-                this.imageUrl = null;
+                this.imageCover = null;
+                this.imageCoverUrl = null;
                 alert('Silahkan Pilih Gambar!');
             }
         },
-        addArticle(){
+        onFileChangeImageArticleFile(event) {
+            const file = event.target.files[0];
+            if (file && file.type.startsWith('image/')) {
+                this.imageArticle.push(file);
+
+                const reader = new FileReader();
+                reader.onload = (e) => {
+                    this.imageArticleUrl.push(e.target.result);
+                };
+
+                reader.readAsDataURL(file);
+            }
+        },
+        deleteImageArticle(index) {
+            console.log(index);
+            this.imageArticle.splice(index, 1);
+            this.imageArticleUrl.splice(index, 1);
+        },
+        storeArticle() {
             const formData = new FormData();
             formData.append("title", this.title);
             formData.append("body", this.body);
             formData.append("user_id", this.user_id);
-            formData.append("image", this.image);
+            formData.append("image", this.imageCover);
 
             axios.post("/api/author/article", formData, {
                 headers: {
                     Authorization: `Bearer ${this.token}`
                 }
             })
-                .then(({ data }) => {
+                .then(({data}) => {
+                    const articleId = data.data.id;
+                    console.log(articleId);
                     console.log(data);
+                    if (this.imageArticle.length !== 0) {
+                        const formData = new FormData();
+
+                        this.imageArticle.forEach((imageFile) => {
+                            formData.append("image[]", imageFile);
+                        });
+
+                        formData.append("article_id", articleId);
+                        axios.post("/api/author/images/article", formData, {
+                            headers: {
+                                Authorization: `Bearer ${this.token}`
+                            }
+                        }).then(({data}) => {
+                            console.log(data)
+                        }).catch(({response}) => {
+                            console.error(response);
+                        });
+                    }
+
+
                 })
-                .catch(({ response }) => {
+                .catch(({response}) => {
                     console.error(response)
                 });
         }
@@ -68,29 +110,65 @@ export default {
             <div class="flex flex-row">
                 <div class="basis-3/4">
                     <div class="mb-2">
-                        <label for="title_article" class="block mb-2 text-sm font-medium text-gray-900">Judul Artikel</label>
-                        <input v-model="title" type="text" id="title_article" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Hello World" required />
+                        <label for="title_article" class="block mb-2 text-sm font-medium text-gray-900">Judul
+                            Artikel</label>
+                        <input v-model="title" type="text" id="title_article"
+                               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                               placeholder="Hello World" required/>
                     </div>
                     <div class="mb-2">
-                        <label for="content_article" class="block mb-2 text-sm font-medium text-gray-900">Konten Artikel</label>
+                        <label for="content_article" class="block mb-2 text-sm font-medium text-gray-900">Konten
+                            Artikel</label>
                         <QuillEditor v-model:content="body" content-type="html" id="content_article" theme="snow"/>
                     </div>
+                    <div class="mb-2">
+                        <label for="image_article" class="block mb-2 text-sm font-medium text-gray-900">Gambar
+                            Artikel</label>
+                        <input type="file" name="image_article" id="image_article" accept="image/*"
+                               @change="onFileChangeImageArticleFile">
+                        <div class="grid grid-cols-3 gap-4">
+                            <template v-if="imageArticleUrl.length !== 0">
+                                <template v-for="(imageUrl, index) in imageArticleUrl">
+                                    <div class="relative">
+                                        <button type="button" @click="deleteImageArticle(index)"
+                                                class="absolute top-1 right-1 bg-transparent items-center justify-center cursor-pointer">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                 stroke-width="1.5" stroke="currentColor"
+                                                 class="w-6 h-6 text-red-400 font-bold">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                      d="M6 18 18 6M6 6l12 12"/>
+                                            </svg>
+                                        </button>
+                                        <img :src="imageUrl" alt="" srcset="" class="w-full max-h-min rounded-lg">
+                                    </div>
+
+                                </template>
+                            </template>
+                        </div>
+                    </div>
                     <div>
-                        <button type="button" @click="addArticle" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Simpan</button>
+                        <button type="button" @click="storeArticle"
+                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 cursor-pointer">
+                            Simpan
+                        </button>
                     </div>
                 </div>
                 <div class="basis-1/4">
-                    <label for="cover_article" class="block mb-2 text-sm font-medium text-gray-900">Cover Artikel</label>
+                    <label for="cover_article" class="block mb-2 text-sm font-medium text-gray-900">Cover
+                        Artikel</label>
                     <div class="p-2">
-                        <template v-if="imageUrl==null">
-                            <img src="../../../../../public/assets/img/empty-img.png" alt="" srcset="" class="w-full rounded-lg">
+                        <template v-if="imageCoverUrl==null">
+                            <img src="../../../../../public/assets/img/add-image.png" alt="" srcset=""
+                                 class="w-full rounded-lg">
                         </template>
                         <template v-else>
-                            <img :src="imageUrl" alt="" srcset="" class="w-full rounded-lg">
+                            <img :src="imageCoverUrl" alt="" srcset="" class="w-full rounded-lg">
                         </template>
                     </div>
                     <div class="p-2">
-                        <input type="file" id="cover_article" name="cover_article" accept="image/*" @change="onFileChange" class="block w-full h-10 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none">
+                        <input type="file" id="cover_article" name="cover_article" accept="image/*"
+                               @change="onFileChangeCoverFile"
+                               class="block w-full h-10 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none">
                     </div>
                 </div>
             </div>
