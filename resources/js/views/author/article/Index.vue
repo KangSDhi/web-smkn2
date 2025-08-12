fb02af7f4f7d1e4f3b9edc194960197b78c5685c
KangSDhi <kangteknisi@gmail.com>
2025-08-12 12:18:02 +0700

    feat(dev): fitur hapus artikel ok

--- resources/js/views/author/article/Index.vue
+++ resources/js/views/author/article/Index.vue
@@ -29,6 +29,11 @@ export default {
             sorted: {
                 field: 'updated_at',
                 rule: 'asc'
+            },
+            isDeleteDialog: false,
+            deleteDataArticle: {
+                id: null,
+                title: null
             }
         }
     },
@@ -167,6 +172,37 @@ export default {
             const getPart = (type) => parts.find(p => p.type === type)?.value;
 
             return `${getPart('day')}-${getPart('month')}-${getPart('year')} ${getPart('hour')}:${getPart('minute')}:${getPart('second')}`;
+        },
+        showDeleteDialog(id){
+            console.log(id);
+            axios.get(`/api/author/article/${id}`, {
+                headers: {
+                    Authorization: `Bearer ${this.token}`
+                }
+            })
+                .then(({ data }) => {
+                    const responseData = data.data;
+                    this.deleteDataArticle.id = responseData.id;
+                    this.deleteDataArticle.title = responseData.title;
+                    this.isDeleteDialog = true;
+                })
+                .catch(({ response }) => {
+                    console.error(response);
+                })
+        },
+        deleteArticle(id){
+            axios.delete(`/api/author/article/${id}`, {
+                headers: {
+                    Authorization: `Bearer ${this.token}`
+                }
+            })
+                .then(({ data }) => {
+                    console.log(data);
+                    this.$router.go();
+                })
+                .catch(({ response }) => {
+                    console.error(response);
+                });
         }
     }
 }
@@ -267,7 +303,7 @@ export default {
                             <td class="px-4 py-4 text-sm">
                                 {{ formatDatetime(item.updated_at) }}
                             </td>
-                            <td class="px-4 py-4 space-x-2 text-right">
+                            <td class="flex px-4 py-4 space-x-2 text-right">
                                 <router-link :to="{ name: 'View Article Author Page', params: { slug: item.slug } }" target="_blank"
                                     class="bg-gray-200 px-2.5 py-1 text-gray-900 rounded-md inline-flex items-center cursor-pointer">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
@@ -277,7 +313,6 @@ export default {
                                               d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z"
                                               clip-rule="evenodd"/>
                                     </svg>
-                                    Preview
                                 </router-link>
                                 <router-link :to="{ name: 'Edit Article Author Page', params: { slug: item.slug } }" target="_blank"
                                     class="bg-blue-200 px-2.5 py-1 text-gray-900 rounded-md inline-flex items-center cursor-pointer">
@@ -287,9 +322,8 @@ export default {
                                         <path stroke-linecap="round" stroke-linejoin="round"
                                               d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"/>
                                     </svg>
-                                    Edit
                                 </router-link>
-                                <button
+                                <button @click="showDeleteDialog(item.id)"
                                     class="bg-red-200 px-2.5 py-1 text-gray-900 rounded-md inline-flex items-center cursor-pointer">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
                                          class="w-4 h-4">
@@ -297,7 +331,6 @@ export default {
                                               d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z"
                                               clip-rule="evenodd"/>
                                     </svg>
-                                    Hapus
                                 </button>
                             </td>
                         </tr>
@@ -352,6 +385,31 @@ export default {
                     </li>
                 </ul>
             </nav>
+
+            <div v-show="isDeleteDialog" class="flex justify-center items-center absolute top-0 right-0 bottom-0 left-0">
+                <div class="relative p-4 w-full max-w-md max-h-full">
+                    <div class="relative bg-white rounded-lg shadow-sm">
+                        <button @click="isDeleteDialog=false" type="button" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center cursor-pointer">
+                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                            </svg>
+                            <span class="sr-only">Close modal</span>
+                        </button>
+                        <div class="p-4 md:p-5 text-center">
+                            <svg class="mx-auto mb-4 text-gray-400 w-12 h-12" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                            </svg>
+                            <h3 class="mb-5 text-lg font-normal text-gray-500">{{ deleteDataArticle.title }}</h3>
+                            <button @click="deleteArticle(deleteDataArticle.id)" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center cursor-pointer">
+                                Hapus
+                            </button>
+                            <button @click="isDeleteDialog=false" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 cursor-pointer">
+                                Batal
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </template>
     </BaseLayout>
 </template>
